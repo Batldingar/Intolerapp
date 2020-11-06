6f6ca92c65cc06f781d26c29bcc90cc49490ca2e
Batldingar <baldinger.andreas@gmail.com>
2020-11-06 15:29:41 +0100

    Optimized the search algorithm

--- app/src/main/java/com/baldware/intolerapp/SearchViewListener.java
+++ app/src/main/java/com/baldware/intolerapp/SearchViewListener.java
@@ -24,85 +24,60 @@ public class SearchViewListener implements SearchView.OnQueryTextListener {
         //Start Thread that starts the sorting
         //When thread is finished it should automatically put the text into the listview
         //When this is called again (because of a new letter) then restart the thread with the new string
-        //Somehow also sort the whole thing
         //Give the results back somehow
         //Look for multithreading opportunities
 
-        long startTime = System.nanoTime();
-
         try {
-            search("S");
+            search("Pa");
         } catch (JSONException e) {
             e.printStackTrace();
         }
-
-        long delta = (System.nanoTime() - startTime) / 1000000;
-        Log.d("Main", "Took " + delta + "mils");
 
         return false;
     }
 
     private ArrayList<Pair> search(String pattern) throws JSONException {
         JSONArray jsonArray = new JSONArray(JSONHandler.getJson());
-        String[] products = new String[jsonArray.length()];
-        String[] brands = new String[jsonArray.length()];
         ArrayList<Pair> resultList = new ArrayList<>();
 
+        // Perform search and valuation
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONObject jsonObject = jsonArray.getJSONObject(i);
-            products[i] = jsonObject.getString("name");
-            brands[i] = jsonObject.getString("brand");
-        }
 
-        // Filter the products
-        for (int i = 0; i < products.length; i++) {
+            String product = jsonObject.getString("name");
+            String brand = jsonObject.getString("brand");
+
             String lowerCasePattern = pattern.toLowerCase();
-            String lowerCaseProduct = products[i].toLowerCase();
-            String lowerCaseBrand = brands[i].toLowerCase();
+            String lowerCaseProduct = product.toLowerCase();
+            String lowerCaseBrand = brand.toLowerCase();
 
             boolean productContains = lowerCaseProduct.contains(lowerCasePattern);
             boolean brandContains = lowerCaseBrand.contains(lowerCasePattern);
 
-            String fullName = products[i] + " - " + brands[i];
-            if (productContains && brandContains) {
-                resultList.add(new Pair(fullName, Math.min(lowerCaseProduct.indexOf(lowerCasePattern), lowerCaseBrand.indexOf(lowerCasePattern))));
-            } else {
-                if (productContains) {
-                    resultList.add(new Pair(fullName, lowerCaseProduct.indexOf(lowerCasePattern)));
-                } else if (brandContains) {
-                    resultList.add(new Pair(fullName, lowerCaseBrand.indexOf(lowerCasePattern)));
+            // Only check if either product or brand contain the pattern
+            if(productContains || brandContains) {
+                String fullName = product + " - " + brand;
+                if (productContains && brandContains) {
+                    resultList.add(new Pair(fullName, Math.min(lowerCaseProduct.indexOf(lowerCasePattern), lowerCaseBrand.indexOf(lowerCasePattern))));
+                } else {
+                    // If one doesn't contain the pattern then the other one has to
+                    if (productContains) {
+                        resultList.add(new Pair(fullName, lowerCaseProduct.indexOf(lowerCasePattern)));
+                    } else {
+                        resultList.add(new Pair(fullName, lowerCaseBrand.indexOf(lowerCasePattern)));
+                    }
                 }
             }
         }
 
         // Sort the products
         quickSort(resultList, 0, resultList.size()-1);
-        //slowSort(resultList);
 
-        // List the new
-        for (Pair pair : resultList) {
+        for(Pair pair : resultList) {
             Log.d("Main", "Result: " + pair.first + " Value: " + pair.second);
         }
 
         return resultList;
-    }
-
-    private void slowSort(ArrayList<Pair> resultList) {
-        boolean neededToSwap;
-        do {
-            neededToSwap = false;
-
-            for (int i = 1; i < resultList.size(); i++) {
-                Pair pair1 = resultList.get(i);
-                Pair pair2 = resultList.get(i - 1);
-                if ((Integer) pair1.second < (Integer) pair2.second) {
-                    Pair buffer = pair2;
-                    resultList.set(i - 1, pair1);
-                    resultList.set(i, buffer);
-                    neededToSwap = true;
-                }
-            }
-        } while (neededToSwap);
     }
 
     private void quickSort(ArrayList<Pair> pairArrayList, int low, int high) {
