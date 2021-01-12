aee23be4153cc39623f4aaec53418ed9c6c38343
Batldingar <baldinger.andreas@gmail.com>
2021-01-12 17:48:55 +0100

    Added option to take pictures for the product AdditionActivity

--- app/src/main/java/com/baldware/intolerapp/activities/AdditionActivity.java
+++ app/src/main/java/com/baldware/intolerapp/activities/AdditionActivity.java
@@ -1,16 +1,27 @@
 package com.baldware.intolerapp.activities;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.baldware.intolerapp.customTools.BitmapHandler;
 import com.baldware.intolerapp.customTools.Constants;
@@ -28,6 +39,7 @@ public class AdditionActivity extends AppCompatActivity {
 
     private static String productNameInput;
     private static String productBrandInput;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +49,7 @@ public class AdditionActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new onClickListener());
 
-        ImageView imageView = findViewById(R.id.addition_image_view);
+        imageView = findViewById(R.id.addition_image_view);
         imageView.setOnClickListener(new onImageClickListener());
     }
 
@@ -86,12 +98,12 @@ public class AdditionActivity extends AppCompatActivity {
     private class onImageClickListener implements View.OnClickListener{
         @Override
         public void onClick(View v) {
-            selectImage();
+            showPictureOptions();
         }
     }
 
-    private void selectImage() {
-        final CharSequence[] options = {"Take photo", "Choose from gallery", "Cancel"};
+    private void showPictureOptions() {
+        final CharSequence[] options = {"Take picture", "Choose from gallery", "Cancel"};
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
@@ -101,13 +113,16 @@ public class AdditionActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int itemID) {
                 switch(itemID) {
-                    case 0: // take photo
-                        Intent takePhoto = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                        startActivityForResult(takePhoto, 0);
+                    case 0: // take picture
+                        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                            takePicture();
+                        } else {
+                            ActivityCompat.requestPermissions(AdditionActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                        }
                         break;
                     case 1: // choose from gallery
-                        Intent choosePhoto = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                        startActivityForResult(choosePhoto , 1);
+                        Intent choosePicture = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        startActivityForResult(choosePicture , 1);
                         break;
                     case 2: // cancel
                         dialog.dismiss();
@@ -117,7 +132,60 @@ public class AdditionActivity extends AppCompatActivity {
         }).show();
     }
 
+    private void takePicture() {
+        Intent takePicture = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePicture, 0);
+    }
 
+    // After calling startActivityForResults in takePicture()
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_CANCELED) {
+            switch (requestCode) {
+                case 0: // take photo
+                    if (resultCode == RESULT_OK && data != null) {
+                        Bitmap selectedImage = (Bitmap) data.getExtras().get("data");
+                        imageView.setImageBitmap(selectedImage);
+                    }
+                    break;
+                case 1: // choose from gallery
+                    if (resultCode == RESULT_OK && data != null) {
+                        Uri selectedImage =  data.getData();
+                        String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                        if (selectedImage != null) {
+                            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+                            if (cursor != null) {
+                                cursor.moveToFirst();
+                                int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+                                String picturePath = cursor.getString(columnIndex);
+                                imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                                cursor.close();
+                            }
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    // After calling ActivityCompat.requestPermissions in case 0 of showPictureOptions
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                } else {
+                    Toast.makeText(AdditionActivity.this, "Camera permissions denied: Unable to take a picture.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
 
     public static String getProductName() {
         return productNameInput;
