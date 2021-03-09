aff385a1e8224604d3c08dabd48ff59c5f4f403b
Batldingar <baldinger.andreas@gmail.com>
2021-03-09 17:01:05 +0100

    Updated privacy policy

--- app/src/main/java/com/baldware/intolerapp/activities/ProductActivity.java
+++ app/src/main/java/com/baldware/intolerapp/activities/ProductActivity.java
@@ -183,16 +183,18 @@ public class ProductActivity extends AppCompatActivity {
     }
 
     private void setColor(RatingBar ratingBar) {
-        if (ratingBar.getRating() <= 1.0) {
-            ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsRed)));
-        } else if (ratingBar.getRating() <= 2.0) {
-            ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsOrange)));
-        } else if (ratingBar.getRating() <= 3.0) {
-            ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsYellow)));
-        } else if (ratingBar.getRating() <= 4.0) {
-            ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsLime)));
-        } else {
-            ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsGreen)));
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (ratingBar.getRating() <= 1.0) {
+                ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsRed)));
+            } else if (ratingBar.getRating() <= 2.0) {
+                ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsOrange)));
+            } else if (ratingBar.getRating() <= 3.0) {
+                ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsYellow)));
+            } else if (ratingBar.getRating() <= 4.0) {
+                ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsLime)));
+            } else {
+                ratingBar.setProgressTintList(ColorStateList.valueOf(ContextCompat.getColor(getApplicationContext(), R.color.starsGreen)));
+            }
         }
     }
 
